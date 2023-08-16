7f67771a38862128f35ccb55cf6323bfde2221c5
RudyWang <a86742902@gmail.com>
2023-08-16 13:54:52 +0800

    feat: 新增 完成當有Group時 regex應該要重新處理

--- main.go
+++ main.go
@@ -40,34 +40,97 @@ func main() {
 	}
 
 	// 定義正則表達式
-	re := regexp.MustCompile(`router\.([A-Z]+)\s*\("(/?[^"]+)",\s*\w+\)`)
+	//"/Attendee/Select"
+	// reForNameAndActions := regexp.MustCompile(`\.([A-Z]+)\s*\("(/?[^"]+)",\s*\w+\)`)
 
-	matches := re.FindAllStringSubmatch(string(content), -1)
-	for _, match := range matches {
-		if len(match) == 3 {
-			httpMethod := match[1]
-			url := match[2]
-			// fmt.Printf("HTTP Method: %s\n", httpMethod)
-			// fmt.Printf("URL: %s\n", url)
-			request := models.Request{}
-			request.Method = httpMethod
-			request.Url = baseUrl + url
-			request.Name = url
-			request.SortNum = 10000
-			request.Created = now
-			request.Modified = now
-			request.Headers = []string{}
-			request.Params = []string{}
-			request.Tests = []string{}
-			exportJson.Requests = append(exportJson.Requests, request)
+	///"getFunctions"
+	// reForActionsWithName := regexp.MustCompile(`\.([A-Z]+)\s*\("([^"]+)",\s*([^)]+)\)`)
+	reForGroup := regexp.MustCompile(`(.+)\s:=\s\w+\.Group\(\"(.+)\"\)`)
+
+	matchesForGroup := reForGroup.FindAllStringSubmatch(string(content), -1)
+	// for _, match := range matches {
+	// 	if len(match) == 3 {
+	// 		httpMethod := match[1]
+	// 		url := match[2]
+	// 		fmt.Printf("HTTP Method: %s\n", httpMethod)
+	// 		fmt.Printf("URL: %s\n", url)
+	// 		// request := models.Request{}
+	// 		// request.Method = httpMethod
+	// 		// request.Url = baseUrl + url
+	// 		// request.Name = url
+	// 		// request.SortNum = 10000
+	// 		// request.Created = now
+	// 		// request.Modified = now
+	// 		// request.Headers = []string{}
+	// 		// request.Params = []string{}
+	// 		// request.Tests = []string{}
+	// 		// exportJson.Requests = append(exportJson.Requests, request)
+	// 	}
+	// }
+
+	// for _, match := range matches {
+	// 	if len(match) == 4 {
+	// 		httpMethod := match[1]
+	// 		url := match[2]
+	// 		handler := match[3]
+	// 		fmt.Printf("HTTP Method: %s\n", httpMethod)
+	// 		fmt.Printf("URL: %s\n", url)
+	// 		fmt.Printf("Handler: %s\n", handler)
+	// 		request := models.Request{}
+	// 		request.Method = httpMethod
+	// 		request.Url = baseUrl + url
+	// 		request.Name = handler
+	// 		request.SortNum = 10000
+	// 		request.Created = now
+	// 		request.Modified = now
+	// 		request.Headers = []string{}
+	// 		request.Params = []string{}
+	// 		request.Tests = []string{}
+	// 		exportJson.Requests = append(exportJson.Requests, request)
+	// 	}
+	// }
+
+	for _, group := range matchesForGroup {
+		if len(group) == 3 {
+			// groupName := group[1]
+			groupRouter := group[2]
+			reForGroupWithActionsAndName := regexp.MustCompile(group[1] + `\.([A-Z]+)\s*\("([^"]+)",\s*([^)]+)\)`)
+			matchesWithGroup := reForGroupWithActionsAndName.FindAllStringSubmatch(string(content), -1)
+			// fmt.Printf("Group Name: %s\n", groupName)
+			// fmt.Printf("Group Router: %s\n", groupRouter)
+
+			for _, route := range matchesWithGroup {
+				if len(route) == 4 {
+					httpMethods := route[1]
+					apiRoutes := route[2]
+					handler := route[3]
+					if apiRoutes[0:1] != "/" {
+						apiRoutes = "/" + apiRoutes
+					}
+
+					// fmt.Printf("Http Methods: %s\n", httpMethods)
+					// fmt.Printf("Api Routes: %s\n", groupRouter+apiRoutes)
+					request := models.Request{}
+					request.Method = httpMethods
+					request.Url = baseUrl + "/" + groupRouter + apiRoutes
+					request.Name = handler
+					request.SortNum = 10000
+					request.Created = now
+					request.Modified = now
+					request.Headers = []string{}
+					request.Params = []string{}
+					request.Tests = []string{}
+					exportJson.Requests = append(exportJson.Requests, request)
+				}
+			}
 		}
 	}
 
 	jsonData, _ := json.Marshal(exportJson)
 
-	// fmt.Println(string(jsonData))
+	fmt.Println(string(jsonData))
 
-	err = os.WriteFile("output.json", jsonData, 0777)
+	err = os.WriteFile("thunder-collection_"+exportJson.CollectionName+".json", jsonData, 0777)
 	if err != nil {
 		log.Fatal("Error writing JSON file:", err)
 		return
