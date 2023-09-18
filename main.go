777b1d8948f720a51519a0518a0673f3e884c553
RudyWang <a86742902@gmail.com>
2023-09-18 17:45:42 +0800

    feat: 測試第一版完成

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Print(`Usage: main <arguments> <ip> <CollectionName>
--t                  convert to thunder-client
--p                  convert to postman
--s                  convert to swagger`)
+		fmt.Print(`
+GoToPost gtp is a tool that generate router's url to json for Thunder Client or Postman.
+
+Usage: 
+
+        gtp <arguments> <ip> <CollectionName>
+
+Arguments:
+
+        -t        Convert to Thunder Client
+        -p        Convert to Postman
+Please visit https://github.com/Rudy1021/goToPost for more information.`)
 		return
 	}
 
@@ -20,7 +28,6 @@ func main() {
 		runner.UseThunder()
 	case "-p":
 		runner.UsePostman()
-	case "-s":
 	default:
 		fmt.Print(`Usage: main <arguments> <ip> <CollectionName>
 		-t                  convert to thunder-client
