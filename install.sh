6b62c6b92206b683481a58ade1a6e2817aa4d288
RudyWang <a86742902@gmail.com>
2023-09-25 14:52:06 +0800

    docs: 修改 完成install.sh

--- install.sh
+++ install.sh
@@ -7,22 +7,23 @@ OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
 # Set the release version.
-VERSION="v0.1.3"
+TAG=$(curl -s "https://api.github.com/repos/Rudy1021/GoToPost/releases/latest" | grep -o '"tag_name": ".*"' | cut -d '"' -f 4)
+VERSION="${TAG#v}"
 
 # Define the URLs for different operating systems and architectures.
 if [ "$OS" = "darwin" ]; then
-  URL="https://github.com/Rudy1021/goToPost/releases/download/${VERSION}/gtp"
+  URL="https://github.com/Rudy1021/goToPost/releases/download/v${VERSION}/goToPost_${VERSION}_darwin_amd64.tar.gz"
 elif [ "$OS" = "linux" ]; then
   if [ "$ARCH" = "x86_64" ]; then
-    URL="https://github.com/Rudy1021/goToPost/releases/download/${VERSION}/gtp"
+    URL="https://github.com/Rudy1021/goToPost/releases/download/v${VERSION}/goToPost_${VERSION}_linux_amd64.tar.gz"
   elif [ "$ARCH" = "arm64" ]; then
-    URL="https://github.com/Rudy1021/goToPost/releases/download/${VERSION}/gtp"
+    URL="https://github.com/Rudy1021/goToPost/releases/download/v${VERSION}/goToPost_${VERSION}_linux_arm64.tar.gz"
   else
     echo "Unsupported architecture: $ARCH"
     exit 1
   fi
 elif [ "$OS" = "windows" ]; then
-  URL="https://github.com/Rudy1021/goToPost/releases/download/${VERSION}/gtp.exe"
+  URL="https://github.com/Rudy1021/goToPost/releases/download/${VERSION}/goToPost_${VERSION}_windows_amd64.exe"
 else
   echo "Unsupported operating system: $OS"
   exit 1
@@ -33,7 +34,10 @@ INSTALL_DIR="$(go env GOPATH)/bin"
 
 # Download and install the binary.
 echo "Downloading and installing GoToPost..."
-curl -sSfL "$URL" -o "$INSTALL_DIR/gtp"
-chmod +x "$INSTALL_DIR/gtp"
+# curl -sSfL "$URL" -o "$INSTALL_DIR/gtp"
 
+curl -LJO $URL
+tar -xzvf goToPost_0.1.3_darwin_amd64.tar.gz -C $INSTALL_DIR
+mv $INSTALL_DIR/GoToPost $INSTALL_DIR/gtp
+rm ./goToPost_0.1.3_darwin_amd64.tar.gz
 echo "GoToPost installed successfully to $INSTALL_DIR"
